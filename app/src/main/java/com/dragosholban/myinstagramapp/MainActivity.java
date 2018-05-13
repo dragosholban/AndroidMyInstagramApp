aecb3125c2ab2acbb951de8886fa8e63af6fac97
Dragos Holban <dragos@intelligentbee.com>
2018-05-13 08:46:57 +0300

    Using Firebase Auth to Authenticate Users

--- app/src/main/java/com/dragosholban/myinstagramapp/MainActivity.java
+++ app/src/main/java/com/dragosholban/myinstagramapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.dragosholban.myinstagramapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view) {
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Authenticated as " + fbUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+            } else {
+                // Sign in failed, check response for error code
+                if (response != null) {
+                    Toast.makeText(this, response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }

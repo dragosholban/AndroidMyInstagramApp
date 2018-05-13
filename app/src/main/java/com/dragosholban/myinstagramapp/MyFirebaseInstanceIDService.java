72598ab496f3be38ec6e59652633d94aacfc2386
Dragos Holban <dragos@intelligentbee.com>
2018-05-13 10:30:59 +0300

    Sending Push Notifications Using Firebase Functions

---
+++ app/src/main/java/com/dragosholban/myinstagramapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,46 @@
+package com.dragosholban.myinstagramapp;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            User user = new User(firebaseUser.getUid(), firebaseUser.getDisplayName(), token);
+            DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+            database.child("users").child(user.uid).setValue(user);
+        }
+    }
+}
